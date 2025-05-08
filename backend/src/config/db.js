c33b99a61ac54951fa741009ec67175f96fba73e
zshfz <zshfz8634@gmail.com>
2025-05-08 15:26:22 +0900

    db 설정, url 설정

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2"); // mysql 플로그인 호출
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "zshfz8076", // 실제 비밀번호로 변경
+  host: "grabthecrown.mysql.database.azure.com",
+  user: "zshfz8634",
+  password: "ljungmin8076!", // 실제 비밀번호로 변경
   database: "crown_hunt",
 });
 
